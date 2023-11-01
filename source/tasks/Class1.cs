8ea3961b8e31adb2908e433dbb3a2d38f2326a44
simplexidev <simplexidev@outlook.com>
2023-11-01 19:56:09 -0400

    Large update, it actually works now! (v2.1.0)

--- source/tasks/Class1.cs
+++ source/tasks/Class1.cs
@@ -1,19 +1,11 @@
 /***********************************************************************************************************************
 * Repository: https://github.com/simplexidev/sdbuildsdk                                                                *
 * Package:    SimplexiDev.Build.Sdk                                                                                    *
-* File:       /source/ProjectType.cs                                                                                   *
+* File:       /source/Class1.cs                                                                                   *
 * License:    https://github.com/simplexidev/sdbuildsdk/blob/main/LICENSE.md                                           *
 ***********************************************************************************************************************/
 
 namespace SimplexiDev.Build.Tasks
 {
-    public enum ProjectType
-    {
-        Library,
-        Tests,
-        Generator,
-        Tool,
-        //Analyzer,
-        //Sdk,
-    }
+    internal static class Class1 { }
 }
