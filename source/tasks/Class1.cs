8438288014c3b1884fc2eacf74609d4366df9ec9
simplexidev <simplexidev@outlook.com>
2024-05-15 23:04:44 -0400

    Lots of updates.

--- source/tasks/Class1.cs
+++ source/tasks/Class1.cs
@@ -1,11 +1,14 @@
 /***********************************************************************************************************************
 * Repository: https://github.com/simplexidev/sdbuildsdk                                                                *
 * Package:    SimplexiDev.Build.Sdk                                                                                    *
-* File:       /source/Class1.cs                                                                                   *
+* File:       /source/Class1.cs                                                                                        *
 * License:    https://github.com/simplexidev/sdbuildsdk/blob/main/LICENSE.md                                           *
 ***********************************************************************************************************************/
 
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
 namespace SimplexiDev.Build.Tasks
 {
-    internal static class Class1 { }
+    internal sealed class Class1 { }
 }
